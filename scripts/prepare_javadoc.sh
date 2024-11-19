9ba1665d03330cd573d08bda0e31c5e24cf03ff7
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2024-11-19 14:37:33 +0100

    fix(scripts): update repository URL in prepare_javadoc.sh

--- scripts/prepare_javadoc.sh
+++ scripts/prepare_javadoc.sh
@@ -14,7 +14,7 @@ fi
 echo "Computed current API version: $version"
 
 echo "Clone $repository_name..."
-git clone https://github.com/eclipse-keypop/$repository_name.git
+git clone https://github.com/jeanpierrefortune/$repository_name.git
 
 cd $repository_name
 
